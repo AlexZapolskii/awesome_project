882f2ce94a696237d5c23db1143c0fc3e6b091bc
Alex Zapolskii <th1rt3n@yandex.ru>
2023-07-27 18:26:15 +0300

    fix: typo

--- src/inference_first.py
+++ src/inference_first.py
@@ -7,7 +7,7 @@ from src.utils.load_data import  load_transform_dataset
 from src.models.optimizer import optimize
 
 
-if __name__ == '__main__'
+if __name__ == '__main__':
     PATH = "data/raw/total_df.csv"
     df = load_transform_dataset(PATH)
 
